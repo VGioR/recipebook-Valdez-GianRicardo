239c41d1de71343d04446c4437fb44f9338295c9
Gio <@Gio>
2026-02-19 18:15:11 +0800

    modified the parameter in reverse

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
         return 'Ingredient: {}'.format(self.name)
     
     def get_absolute_url(self):
-        return reverse("Ingredient_detail", args=[self.id])
+        return reverse("ledger:ingredient_detail", args=[self.id])
     
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
@@ -18,7 +18,7 @@ class Recipe(models.Model):
         return 'Recipe: {}'.format(self.name)
     
     def get_absolute_url(self):
-        return reverse("recipe_detail", args=[self.id])
+        return reverse("ledger:recipe_detail", args=[self.id])
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
