4fc034c43a804775643e2cae38f8d7273616595f
Gio <@Gio>
2026-02-19 17:51:36 +0800

    made the ctx for recipe_detail refer to a singular recipe

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -6,13 +6,11 @@ from django.views.generic.detail import DetailView
 
 def recipe_list(request):
 	recipes = Recipe.objects.all()
-	ctx = {
-		"recipes" : recipes
-	}
+	ctx = {"recipes" : recipes}
 	return render(request, 'recipe_list.html', ctx)
 
 def recipe_detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
-    ctx = { "recipes" : recipe}
+    ctx = {"recipe" : recipe}
 
     return render(request, 'recipe.html', ctx)
