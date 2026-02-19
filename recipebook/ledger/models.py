c5920c99b67bd5e10d0405a8281f2f8fbcf2d2dc
Gio <@Gio>
2026-02-19 10:21:47 +0800

    created models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,34 @@
+from datetime import datetime
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Ingredient: {}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse("Ingredient_detail", args=[str(self.name)])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Recipe: {}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse("Recipe_detail", args=[str(self.name)])
+    
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE,
+        related_name='Ingredients'
+    )
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE,
+        related_name='Recipes'
+    )
