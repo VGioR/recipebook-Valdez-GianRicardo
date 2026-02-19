05ab93dcd74428b53683615bec9f3adf99e6994e
Gio <@Gio>
2026-02-19 16:16:01 +0800

    created the models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -9,7 +9,7 @@ class Ingredient(models.Model):
         return 'Ingredient: {}'.format(self.name)
     
     def get_absolute_url(self):
-        return reverse("Ingredient_detail", args=[str(self.name)])
+        return reverse("Ingredient_detail", args=[self.id])
     
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
@@ -18,17 +18,17 @@ class Recipe(models.Model):
         return 'Recipe: {}'.format(self.name)
     
     def get_absolute_url(self):
-        return reverse("Recipe_detail", args=[str(self.name)])
+        return reverse("recipe_detail", args=[self.id])
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredient = models.ForeignKey(
         Ingredient, 
         on_delete=models.CASCADE,
-        related_name='Ingredients'
+        related_name='recipe'
     )
     recipe = models.ForeignKey(
         Recipe, 
         on_delete=models.CASCADE,
-        related_name='Recipes'
+        related_name='ingredients'
     )
