3a9f0ed2c6dbb37592ef9ddaa065f32d12ce9b60
Gio <@Gio>
2026-03-02 22:45:08 +0800

    added a validator for 256 characters

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(validators=[MinLengthValidator(256)])
 
     def __str__(self):
         return self.name
