7c6a1aeac860991054a53a5c50691638c78e3d29
Gio <@Gio>
2026-03-02 12:14:36 +0800

    created the Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True)
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
