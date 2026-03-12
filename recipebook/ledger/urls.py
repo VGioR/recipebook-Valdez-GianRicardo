3c24d5699f4e27e2f1d54c2d845e0327ee7fd230
Gio <@Gio>
2026-03-12 14:36:49 +0800

    updated urls with the form to add recipes

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail
+from .views import recipe_list, recipe_detail, recipe_form
 
 urlpatterns = [
     path('recipes/list', recipe_list, name="recipe_list"),
     path('recipe/<int:pk>', recipe_detail, name="recipe_detail"),
+    path('recipe/add', recipe_form, name="recipe_form"),
 ]
 
 app_name = "ledger"
