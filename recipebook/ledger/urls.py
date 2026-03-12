36445c925d2e61418d76c6c8e632670ed6ce9fec
Gio <@Gio>
2026-03-12 20:11:27 +0800

    added a url path for the add image page for each recipe detail

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail, recipe_form
+from .views import recipe_list, recipe_detail, recipe_form, image_form
 
 urlpatterns = [
     path('recipes/list', recipe_list, name="recipe_list"),
     path('recipe/<int:pk>', recipe_detail, name="recipe_detail"),
     path('recipe/add', recipe_form, name="recipe_form"),
+    path('recipe/<int:pk>/add_image', image_form, name="image_form"),
 ]
 
 app_name = "ledger"
