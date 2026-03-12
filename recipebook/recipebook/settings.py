e3b20b3f55e27e3c8a51ac438cff20db3dfa28f0
Gio <@Gio>
2026-03-12 20:07:51 +0800

    added important media settings

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -23,6 +23,8 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
